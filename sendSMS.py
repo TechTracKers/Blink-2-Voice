c782cd82c0f69a58375ad34c47f6d9e74b6f02f3
Ujjwal Prakash <ujjwalprakash3@gmail.com>
2018-09-01 19:00:21 +0530

    update sendSMS.py

--- sendSMS.py
+++ sendSMS.py
@@ -1,26 +1,16 @@
-# from twilio.rest import Client
-# account_sid='ACed38494fdb2dd50505a2df20f8b00c20'
-# auth_token='acf7b4af2a59e57151a061da9c5044e1'
-# client=Client(account_sid,auth_token)
-# client.messages.create(
-#     to='+91 9870216540',
-#     from_='17015436151',
-#     body='Hi this is kumar mayank'
-#     )
-
 from twilio.rest import Client
 
 def send(msg):
-    account_sid="ACed38494fdb2dd50505a2df20f8b00c20"
-    auth_token="acf7b4af2a59e57151a061da9c5044e1"
+    account_sid="your auth_id"
+    auth_token="your auth_token"
     client=Client(account_sid, auth_token)
     if msg:
         client.messages.create(
-        to="+917291872075",
-        from_="+17015436151",
+        to="123456789",
+        from_="123456789",
         body=msg)
     else:
         client.messages.create(
-        to="+917291872075",
-        from_="+17015436151",
-        body="I am at MSHacks 2.0")
+        to="123456789",
+        from_="123456789",
+        body="Hello")
